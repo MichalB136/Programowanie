d354eb74371d0cada3ae7b0a0893f65b4b2764b0
MichalB136 <mbojko136@gmail.com>
2020-09-03 10:34:03 +0200

    wsig edit

--- biblioteka/biblioteka/wsgi.py
+++ biblioteka/biblioteka/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteka.settings')
 
 application = get_wsgi_application()
+sys.path.append('/var/www')
+sys.path.append('/var/www/mysite')
