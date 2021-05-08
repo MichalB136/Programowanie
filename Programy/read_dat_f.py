b15e5ea75c8b4c005ec507c36e3ca2ec549aafc0
Michal <mbojko136@gmail.com>
2021-05-08 12:57:56 +0200

    tak

--- Programy/read_dat_f.py
+++ Programy/read_dat_f.py
@@ -2,6 +2,7 @@
 from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier
 import numpy as np
 import pandas as pd
+import psycopg2
 
 from scipy.sparse import csc_matrix
 from scipy.sparse import csr_matrix
@@ -23,6 +24,52 @@ from MyLossFunction import BinomialDeviance
 from MyRuleFit import MyRuleFit
 
 #%%
+conn = psycopg2.connect("dbname=mimic user=postgres password=Boychan!36 port=5433")
+cur = conn.cursor()
+# cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+# cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+#%%
+cur.execute("""SELECT ie.subject_id, ie.hadm_id, ie.icustay_id,
+    ie.intime, ie.outtime, adm.deathtime,
+    ROUND((cast(ie.intime as date) - cast(pat.dob as date))/365.242, 2) AS age,
+    ROUND((cast(ie.intime as date) - cast(adm.admittime as date))/365.242, 2) AS preiculos,
+    CASE
+        WHEN ROUND((cast(ie.intime as date) - cast(pat.dob as date))/365.242, 2) <= 1
+            THEN 'neonate'
+        WHEN ROUND((cast(ie.intime as date) - cast(pat.dob as date))/365.242, 2) <= 14
+            THEN 'middle'
+        -- all ages > 89 in the database were replaced with 300
+        WHEN ROUND((cast(ie.intime as date) - cast(pat.dob as date))/365.242, 2) > 100
+            THEN '>89'
+        ELSE 'adult'
+        END AS ICUSTAY_AGE_GROUP,
+    -- note that there is already a "hospital_expire_flag" field in the admissions table which you could use
+    CASE
+        WHEN adm.hospital_expire_flag = 1 then 'Y'           
+    ELSE 'N'
+    END AS hospital_expire_flag,
+    -- note also that hospital_expire_flag is equivalent to "Is adm.deathtime not null?"
+    CASE
+        WHEN adm.deathtime BETWEEN ie.intime and ie.outtime
+            THEN 'Y'
+        -- sometimes there are typographical errors in the death date, so check before intime
+        WHEN adm.deathtime <= ie.intime
+            THEN 'Y'
+        WHEN adm.dischtime <= ie.outtime
+            AND adm.discharge_location = 'DEAD/EXPIRED'
+            THEN 'Y'
+        ELSE 'N'
+        END AS ICUSTAY_EXPIRE_FLAG
+     FROM icustays ie
+     INNER JOIN patients pat
+     ON ie.subject_id = pat.subject_id
+     INNER JOIN admissions adm
+     ON ie.hadm_id = adm.hadm_id;
+               """)
+test = cur.fetchall()
+# cur.close()
+# conn.close()
+#%%
 iris = load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = pd.Series(iris.target)
@@ -42,11 +89,6 @@ y_predict = test.predict(X_test)
 #%% 
 rules = test.get_rules()
 #%%
-regresor2 = GradientBoostingClassifier( n_estimators=10)
-test2 = regresor2.fit(X2, y2)
-# test = regresor.pseudo_res(1, y)
-# regresor.fit_stage(1, X, y, 5, None, None, None)
-# regresor.compute(1, y)
 
 
 
